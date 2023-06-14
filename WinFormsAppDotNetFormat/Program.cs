a34b44037857d5cb804acefd131b81c24cec4640
Jeremy Powell <jeremy@visionaid.com>
2023-06-15 09:52:30 +1200

    Add project files.

---
+++ WinFormsAppDotNetFormat/Program.cs
@@ -0,0 +1,21 @@
+using SpeechLib;
+
+namespace WinFormsAppDotNetFormat
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            SpVoice voice = new();
+
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
